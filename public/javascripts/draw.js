28ca1a946c97953e0643ba3979cd0659f823f10a
Aurélien Hervé <aurelien@hunteed.com>
2016-12-27 23:05:05 +0100

    plot humidity

--- public/javascripts/draw.js
+++ public/javascripts/draw.js
@@ -1,8 +1,9 @@
 $.ajax({url: '/data', success: function (rawData) {
   var reports = rawData.reports
   var categories = reports.map(r => new Date(r.date))
-  var data = reports.map(r => parseFloat(r.temperature))
-  Highcharts.chart('container', {
+  var temperature = reports.map(r => parseFloat(r.temperature))
+  var humidity = reports.map(r => parseFloat(r.humidity))
+  Highcharts.chart('temp', {
     title: {
       text: 'Temperature in my room',
       x: -20 //center
@@ -35,7 +36,43 @@ $.ajax({url: '/data', success: function (rawData) {
     },
     series: [{
       name: 'Temperature',
-      data: data
+      data: temperature
+    }]
+  });
+  Highcharts.chart('hum', {
+    title: {
+      text: 'Humidity of my flat',
+      x: -20 //center
+    },
+    subtitle: {
+      text: 'As measured by super fancy arduino setup',
+      x: -20
+    },
+    xAxis: {
+      categories: categories
+    },
+    yAxis: {
+      title: {
+        text: 'Humidity (%)'
+      },
+      plotLines: [{
+        value: 0,
+        width: 1,
+        color: '#808080'
+      }]
+    },
+    tooltip: {
+      valueSuffix: '%'
+    },
+    legend: {
+      layout: 'vertical',
+      align: 'right',
+      verticalAlign: 'middle',
+      borderWidth: 0
+    },
+    series: [{
+      name: 'Temperature',
+      data: humidity
     }]
   });
 }})
